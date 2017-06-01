493ebda9f4a4a558be612186ce01be7bbfd00970
Morgan Pierson <morganpierson@gmail.com>
2017-06-01 13:07:37 -0700

    basic searchbar component

---
+++ src/containers/searchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form className='group'>
+        <input />
+      </form>
+    )
+  }
+}
+
+export default SearchBar;
